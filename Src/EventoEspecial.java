ad57d37ef7afe9b0e357f2e2fe433ef20d396cf8
Caiofontescodigos <cfontes618@gmail.com>
2025-11-26 09:52:55 -0300

    Adição da classe EventoEspecial

---
+++ Src/EventoEspecial.java
@@ -0,0 +1,19 @@
+public class EventoEspecial extends Filme {
+
+    public String titulo;
+    public String tipoEvento;
+    public double precoBase;
+    public int DuracaoMinutos;
+    public String classificacaoIndicativa;
+
+    public EventoEspecial(String titulo, int duracaoMinutos, String classificacaoIndicativa, String tipoEvento, double precoBase) {
+        super(titulo, duracaoMinutos, classificacaoIndicativa);
+        this.tipoEvento = tipoEvento;
+        this.precoBase = precoBase;
+    }
+    @Override
+    public void exibirDetalhes() {
+         String.format("Evento Especial: %s\n  Tipo: %s\n  Duração: %d min\n  Classificação: %s\n  Preço Base: R$ %.2f",
+                titulo, tipoEvento, DuracaoMinutos, classificacaoIndicativa, precoBase);
+    }
+}
