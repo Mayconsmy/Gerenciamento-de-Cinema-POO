e6b8c73b9111ecd53053c4828d6f261d23281a18
Ítallo de Mesquita <itallodemesquita@outlook.com.br>
2025-12-08 02:35:19 +0000

    add: menu de opções do gerenciador de filmes

--- Src/br/com/cinema/gerenciadores/GerenciadorFilme.java
+++ Src/br/com/cinema/gerenciadores/GerenciadorFilme.java
@@ -10,3 +10,21 @@ public class GerenciadorFilme {
     public GerenciadorFilme(Cinema cinema) {
         this.cinema = cinema;
     }
+
+    public void menu() {
+        int opcao = -1;
+        while (opcao != 0) {
+            System.out.println("\n--- GESTÃO DE FILMES ---");
+            System.out.println("1. Listar Filmes");
+            System.out.println("2. Adicionar Filme");
+            System.out.println("0. Voltar");
+            opcao = Utilitarios.lerInt("Opção: ");
+
+            switch (opcao) {
+                case 1 -> listar();
+                case 2 -> adicionar();
+                case 0 -> System.out.println("Voltando...");
+                default -> System.out.println("Opção inválida.");
+            }
+        }
+    }
