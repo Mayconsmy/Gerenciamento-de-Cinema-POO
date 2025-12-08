289cfbc7b65da7b16b51e8d50aaf2904072d2595
Ítallo de Mesquita <itallodemesquita@outlook.com.br>
2025-12-08 02:31:34 +0000

    add: atributos do gerenciador de filmes

--- Src/br/com/cinema/gerenciadores/GerenciadorFilme.java
+++ Src/br/com/cinema/gerenciadores/GerenciadorFilme.java
@@ -1,5 +1,12 @@
 package br.com.cinema.gerenciadores;
 
+import br.com.cinema.entidades.Filme;
+import br.com.cinema.sistema.Cinema;
+import br.com.cinema.utilitarios.Utilitarios;
+
 public class GerenciadorFilme {
-    
-}
+    private Cinema cinema;
+
+    public GerenciadorFilme(Cinema cinema) {
+        this.cinema = cinema;
+    }
